710cab3fdbef6c4c3f9294b2ab2f80549acb1a3f
JiangKevin <JiangKevin@users.noreply.github.com>
2024-08-30 11:31:43 +0800

    期初

---
+++ run.sh
@@ -0,0 +1,15 @@
+# 清屏
+clear
+reset
+#
+export FmDev=$(pwd)
+#
+# rm ./common.data -y
+# rm ./data.js -y
+# file_packager common.data --preload ./web/db@/ --preload ./Data/tt@/Project/tt --js-output=data.js
+file_packager common.data --preload ./web/asset@/ --js-output=data.js
+
+mv ./common.data ./web/common/common.data
+mv ./data.js ./web/common/data.js
+#
+node server.js
